01fdc7ca283cbed651684acf06876be55911280d
Robert <robiv2007@gmail.com>
2022-05-09 16:17:16 +0200

    first commit

---
+++ Comic-Time/Comic_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Comic_TimeApp.swift
+//  Comic-Time
+//
+//  Created by vatran robert on 2022-05-09.
+//
+
+import SwiftUI
+
+@main
+struct Comic_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
