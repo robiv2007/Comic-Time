f2269b96b3bb37a75c160ab3feeb68e808e1d57d
Robert <robiv2007@gmail.com>
2022-05-10 18:06:26 +0200

    viewmodel added,random and search fix

--- Comic-Time/ContentView.swift
+++ Comic-Time/ContentView.swift
@@ -9,77 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var titleValue = String()
-    @State var imageValue = String()
-    @State var numberInput = 1
+    @StateObject var viewModel = ViewModel()
     
     
     var body: some View {
         HStack{
-            TextField("Search number", value:$numberInput , formatter:NumberFormatter())
+            TextField("Search number", text:$viewModel.numberInput)
             Button(action: {
-                fetchAPI()
+                viewModel.fetchAPI()
+                viewModel.numberInput = ""
             }, label: {
                 Text("Get")
             })
             .padding()
         }
         ScrollView{
-            Text(titleValue)
+            Text(viewModel.titleValue)
                 .bold()
-            AsyncImage(url: URL(string: imageValue)){ image in
+            
+            AsyncImage(url: URL(string: viewModel.imageValue)){ image in
                 image.resizable()
             } placeholder: {
                 ProgressView()
             }.aspectRatio(contentMode: .fit)
                 .frame(width:450, height: 500, alignment: .center)
+            Text("Exponetion")
+                .onTapGesture {
+                    UIApplication.shared.open(URL(string:"https://www.explainxkcd.com/wiki/index.php/\(viewModel.number)")!)
+                }
+            
+            
             Button(action: {
-                getRandom()
+                viewModel.fetchAPIRandom()
             }, label: {
                 Text("Random")
             })
             
         }
+        .onAppear(perform: viewModel.fetchAPI)
     }
-    
-    func fetchAPI(){
-        let url = URL(string: "https://xkcd.com/\(self.numberInput)/info.0.json")
-        
-        URLSession.shared.dataTask(with: url!){data, response, error in
-            if let data = data {
-                print("DAAT",data)
-                if let decoder = try? JSONDecoder().decode(ComicStructure.self, from: data){
-                    self.titleValue = decoder.title
-                    self.imageValue = decoder.img
-                    print("TITLE",titleValue)
-                    
-                }
-            }
-            
-        }.resume()
-        
-    }
-    
-    func getRandom(){
-        
-        numberInput = Int.random(in: 1..<2500)
-        fetchAPI()
-        
-    }
-    
-}
-
-struct ComicStructure: Codable {
-    var month : String
-    var num : Int
-    var link: String
-    var year: String
-    var news: String
-    var safe_title: String
-    var alt : String
-    var img: String
-    var title: String
-    var day: String
     
 }
 
@@ -89,3 +57,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
